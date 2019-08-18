5cb92a652152b439bbbf6d70040a210ad6db910c
Burak <97burakfidan97@gmail.com>
2019-08-18 19:48:57 +0300

    Git init.

---
+++ app/src/main/java/com/mrntlu/jetpacknavigationcomponents/ConfirmationFragment.kt
@@ -0,0 +1,32 @@
+package com.mrntlu.jetpacknavigationcomponents
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_confirmation.*
+
+class ConfirmationFragment : Fragment() {
+
+    private lateinit var recipient: String
+    private lateinit var money:Money
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipient=arguments!!.getString("recipient")!!
+        money=arguments!!.getParcelable("amount")!!
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_confirmation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val amount=money.amount
+        val confirmationMessage="You've sent $amount\$ to $recipient"
+        confirmation_message.text=confirmationMessage
+    }
+}
---
+++ app/src/main/java/com/mrntlu/jetpacknavigationcomponents/SpecifyAmountFragment.kt
@@ -0,0 +1,59 @@
+package com.mrntlu.jetpacknavigationcomponents
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_choose_recipient.cancel_btn
+import kotlinx.android.synthetic.main.fragment_specify_amount.*
+import java.math.BigDecimal
+
+class SpecifyAmountFragment : Fragment(),View.OnClickListener {
+
+    private lateinit var navController: NavController
+    private lateinit var recipientBundle:String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipientBundle= arguments!!.getString("recipient")!!
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_specify_amount, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController= Navigation.findNavController(view)
+
+        send_btn.setOnClickListener(this)
+        cancel_btn.setOnClickListener(this)
+        val message="Sending money to $recipientBundle"
+        recipient.text=message
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.send_btn -> {
+                if(!TextUtils.isEmpty(input_amount.text.toString())){
+                    val amount=Money(BigDecimal(input_amount.text.toString()))
+                    val bundle= bundleOf(
+                        "recipient" to recipientBundle,
+                        "amount" to amount
+                    )
+                    navController.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment,bundle)
+                }else{
+                    Toast.makeText(activity,"Enter amount",Toast.LENGTH_SHORT).show()
+                }
+            }
+            R.id.cancel_btn -> activity!!.onBackPressed()
+        }
+    }
+}
